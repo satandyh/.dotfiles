798bb47ac239fdc3baa09a4ebfd57649ff6149ef
Anton Gura <an.gura@corp.vk.com>
2020-10-20 14:39:38 +0300

    added checks new formet for directories

--- bin/dot-bootstrap.sh
+++ bin/dot-bootstrap.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v ansible)" ]; then
   yum install ansible -y
 fi
 
-ansible-playbook -i ~/.dotfiles/hosts.yml ~/.dotfiles/deploy.yml --ask-become-pass --diff
+ansible-playbook -i ~/git/.dotfiles/inventory/hosts.yml ~/git/.dotfiles/deploy.yml --ask-become-pass --diff
 
 #if command -v terminal-notifier 1>/dev/null 2>&1; then
 #  terminal-notifier -title "dotfiles: Bootstrap complete" -message "Successfully set up environment."
